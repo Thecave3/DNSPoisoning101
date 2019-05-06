59b961a7c86d053e32fd6c012a5b506cf01a681c
Andrea Lacava <thecave003@gmail.com>
2019-05-07 00:51:41 +0200

    python
    - bait.py : dns client
    - listener.py : server della victory flag
    - util.py : libreria di supporto
    - venom.py : dns server

--- bait.py
+++ bait.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 """Bait DNS client
 
-This script makes Python bite the DNS rat and gives us total redirect control
+This script makes a DNS request to vulnDNS asking information about DNS_URL_BADGUY
 
 """
 
 import socket
 
-from util import dns_query_builder
+from util import dns_request_builder
 
-TARGET_IP = "8.8.8.8"  # vulnDNS' IP
+TARGET_IP = "8.8.8.8"  # vulnDNS's IP
 TARGET_PORT = 53
 DNS_URL_BADGUY = "www.andrealacava.com"
 
@@ -18,7 +18,7 @@ DNS_URL_BADGUY = "www.andrealacava.com"
 def main():
     # the socket will be used on internet (AF_INET) to send an UDP datagram (SOCK_DGRAM)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    dns_query = dns_query_builder(DNS_URL_BADGUY)
+    dns_query = dns_request_builder(DNS_URL_BADGUY)
     sock.sendto(dns_query, (TARGET_IP, TARGET_PORT))
     sock.close()
 
--- listener.py
+++ listener.py
@@ -1,14 +1,28 @@
-import socket
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# this script is just for
+"""Listener UDP server
+
+This script listens on port 1337 the Victory flag.
+
+"""
+
+from socket import *
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 1337
+BUFFER_SIZE = 1024
 
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    print("received message:", data)
+def main():
+    sock = socket(AF_INET,  # Internet
+                  SOCK_DGRAM)  # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        print("Received from %s : %s", addr, data)
+
+
+if __name__ == "__main__":
+    main()
